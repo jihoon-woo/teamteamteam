9b43a12bf36e1d80b21e178b7ec88228bae8acbf
charlie <charlie2019370@yonsei.ac.kr>
2023-11-09 11:24:50 +0900

    change name

--- QD_DETR_m/qd_detr/standalone_eval/eval.py
+++ QD_DETR_m/qd_detr/standalone_eval/eval.py
@@ -4,7 +4,7 @@ import json
 import time
 import copy
 import multiprocessing as mp
-from utils import compute_average_precision_detection, \
+from eval_utils import compute_average_precision_detection, \
     compute_temporal_iou_batch_cross, compute_temporal_iou_batch_paired, load_jsonl, get_ap
 
 
